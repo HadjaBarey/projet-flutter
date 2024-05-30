32e79cce87bc56bd6d40558b2e52050613712e68
HadjaBary <hadjaratouyougbare07@gmail.com>
2024-05-30 09:47:54 +0000

    first commit

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -11,23 +11,30 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: 300.0, // Ajustez la largeur du cadre selon vos besoins
-          height: 100.0, // Ajustez la hauteur du cadre selon vos besoins
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10.0),
-            child: AppBar(
-              title: Text('KADOUS TRANSFERT'),
-              centerTitle: true,
+        body: Align(
+          alignment: Alignment.topCenter, // Aligne le cadre en haut, centré horizontalement
+          child: Container(
+            width: 500.0, // Ajustez la largeur du cadre selon vos besoins
+            height: 90.0, // Ajustez la hauteur du cadre selon vos besoins
+            decoration: BoxDecoration(
+              color: Colors.orange, // Définit la couleur de fond du cadre
+              // borderRadius: BorderRadius.circular(10.0), // Ajoute des coins arrondis au cadre
+            ),
+            child: Center(
+              child: Text(
+                'KADOUS TRANSFERT',
+                style: TextStyle(
+                  color: Colors.white, // Couleur du texte
+                  fontSize: 27.0, // Taille de la police
+                  fontWeight: FontWeight.bold, // Met le texte en gras
+                ),
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+
+
+
   }
 }
